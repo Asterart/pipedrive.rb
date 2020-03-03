877ea4a3d3eaa1dbd4e966139f4455e31cc3d388
Asterart <peter.jacob.jeszke@gmail.com>
2020-03-03 11:59:51 +0100

    move get deals by pipeline to pipeline

--- lib/pipedrive/deal.rb
+++ lib/pipedrive/deal.rb
@@ -4,9 +4,5 @@ module Pipedrive
     include ::Pipedrive::Operations::Read
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
-
-    def find_by_pipeline_id(id)
-      make_api_call_for_deals(id)
-    end
   end
 end
--- lib/pipedrive/pipeline.rb
+++ lib/pipedrive/pipeline.rb
@@ -4,5 +4,9 @@ module Pipedrive
     include ::Pipedrive::Operations::Create
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
+
+    def find_deals_by_pipeline_id(id)
+      make_api_call_for_deals(id)
+    end
   end
 end
